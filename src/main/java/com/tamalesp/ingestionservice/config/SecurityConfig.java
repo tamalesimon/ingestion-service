5c22eb6203860296a4f0e1728de2e7379dcb817b
tamalesimon <tamalesp@gmail.com>
2025-09-23 23:16:46 +0300

    refactored the service to use resource issuer for security

---
+++ src/main/java/com/tamalesp/ingestionservice/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.tamalesp.ingestionservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange-> exchange
+                        .pathMatchers(HttpMethod.POST, "/api/v1/logs").hasAuthority("SCOPE_logs:write")
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+
+    }
+}
